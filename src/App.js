45d76ad850b6b6c4930668d1b21d11939e667e26
Dominic Yegon <dyegon@maliora.tech>
2021-04-07 18:43:33 +0300

    Changed some text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React is Awesome
+          Learn React
         </a>
       </header>
     </div>
